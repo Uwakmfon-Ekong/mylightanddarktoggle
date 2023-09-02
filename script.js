ef72af79d054ec9375d57b972660e45a300d8d61
Uwakmfon-Ekong <uwakmfone470@gmail.com>
2023-09-02 09:52:33 +0100

    first commit

---
+++ script.js
@@ -0,0 +1,16 @@
+const toggle = document.querySelector("#toggleDark");
+const body = document.querySelector("body");
+
+toggle.addEventListener("click", function () {
+    this.classList.toggle("fa-moon");
+    if (this.classList.toggle("fa-sun")) {
+        body.style.background = "white";
+        body.style.color = "black";
+        body.style.transition = "2s";
+    }
+    else {
+        body.style.background = "black";
+        body.style.color = "white";
+        body.style.transition = "2s";
+    }
+})
